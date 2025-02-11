4ae24503420f0d2ecd54a9cf1af2c078328c4071
KAAL <104773744+Bikram0077@users.noreply.github.com>
2025-02-11 18:53:45 +0545

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,19 @@ import Clients from './components/Clients';
 import Contact from './components/Contact';
 import AdminLogin from './components/admin/AdminLogin';
 import AdminDashboard from './components/admin/AdminDashboard';
+import Head from 'next/head';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
 
 function App() {
   return (
